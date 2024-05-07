e354bd5c7235638e40742728ce183e44554af7d1
Максим Лемешев <leshuj@gmail.com>
2024-05-08 00:25:31 +0300

    first project

--- js/script.js
+++ js/script.js
@@ -1,44 +1,22 @@
 "use strict";
 
-// let a = 15;
-// console.log(a);
-// let number = 5;
-//
-// const leftBorderWidth = 1;
-//
-// number = 10;
-// console.log(number);
-//
-// const obj = {
-//     a: 50
-// }
-//
-// obj.a = 10;
-//
-// console.log(obj);
-// console.log(name);
-//
-// var name = 'Ivan';
-//
+const numberOfFilms = +prompt('Сколько ильмов вы посмотрели?', '');
 
-// alert('hello');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const result = confirm('Done?');
+const lastViewedMovie = prompt('Последний просмотренный фильм?');
+const movieRating = +prompt("На сколько оцените его?");
 
-// console.log(result);
+personalMovieDB.movies[lastViewedMovie] = movieRating;
 
-// const  answer = +prompt("Are you 19?", "33");
-//
-// console.log(typeof(answer));
+const lastViewedMovie2 = prompt('Последний просмотренный фильм?');
+const movieRating2 = +prompt("На сколько оцените его?");
 
-// const answers = [];
-//
-// answers[0] = prompt('Name?' , "");
-// answers[1] = prompt('Age?' , "");
-// answers[2] = prompt('City?' , "");
-//
-// document.write(typeof (answers));
-
-const category  = 'toys';
-
-console.log(4 + " - object");
+personalMovieDB.movies[lastViewedMovie2] = movieRating2;
+console.log(personalMovieDB);
