e66b1f8c5a63de0aa1c008f212033705cf808bd7
Максим Лемешев <lemeshev@aweb.ua>
2024-05-07 22:08:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// let a = 15;
+// console.log(a);
+// let number = 5;
+//
+// const leftBorderWidth = 1;
+//
+// number = 10;
+// console.log(number);
+//
+// const obj = {
+//     a: 50
+// }
+//
+// obj.a = 10;
+//
+// console.log(obj);
+// console.log(name);
+//
+// var name = 'Ivan';
+//
+
+// alert('hello');
+
+// const result = confirm('Done?');
+
+// console.log(result);
+
+// const  answer = +prompt("Are you 19?", "33");
+//
+// console.log(typeof(answer));
+
+// const answers = [];
+//
+// answers[0] = prompt('Name?' , "");
+// answers[1] = prompt('Age?' , "");
+// answers[2] = prompt('City?' , "");
+//
+// document.write(typeof (answers));
+
+const category  = 'toys';
+
+console.log(4 + " - object");
